59003667e9ae3dbb86c454ffff0e57f70292a5fc
Crescenzo De Martino <cresckamina@gmail.com>
2020-06-25 14:43:56 +0200

    Finalizzati commenti in js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
 $(document).ready(
   function () {
     var numeroQuadratini = 36
+    // Nel caso si voglia customizzare la quantità di quadratini de-commentare la riga sottostante
     // var numeroQuadratini = parseInt(prompt('Quanti quadratini totali vuoi creare? (Per l\' esercizio digitare 36)'))
     for (var i = 0; i < numeroQuadratini; i++) {
       $('.container').append('<div class="quadratino"></div>')
     }
-
+    // Aggiungo funzionalità al click sul singolo quadratino
     $(document).on('click','.quadratino',function () {
       var quelloLi = $(this)
+      // Reset in caso di re-click
       $(quelloLi).removeClass('yellow').removeClass('green')
-
+      // Collegamento richiesta ajax
       $.ajax(
         {
           url: 'https://flynn.boolean.careers/exercises/api/random/int',
@@ -18,6 +20,7 @@ $(document).ready(
           success: function (data) {
             var numeroRandom = data.response
             $(quelloLi).text(numeroRandom)
+            // Imposto condizione che regola il colore finale del quadratino a seconda del numero
             if (numeroRandom <= 5) {
               $(quelloLi).addClass('yellow')
             } else {
@@ -26,7 +29,6 @@ $(document).ready(
           }
         }
       )
-
     })
   }
 )
